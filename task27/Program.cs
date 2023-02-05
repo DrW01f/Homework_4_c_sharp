42a666956bf2dc7f68a9d072bd33a46ebff6fb1e
Andrey <Dr_Wo1f@mail.ru>
2023-02-05 23:18:19 +0300

    task 27 error

--- task27/Program.cs
+++ task27/Program.cs
@@ -5,28 +5,60 @@
 // 9012 -> 12
 
 
-int Input()
-{
-    Console.WriteLine("Введите число");
-    int count = int.Parse(Console.ReadLine()!);
-    return count;
-}
+// int Input()
+// {
+//     Console.WriteLine("Введите число");
+//     int count = int.Parse(Console.ReadLine()!);
+//     return count;
+// }
 
-int magic(int number)
+// int Counter(int number) //вычисляю порядок числа (начало с 1)
+// {
+//     int count = 1;
+//     for (int del = 10; number / del > 0; del *= 10)
+//     {
+//         count++;
+//     }
+//     return count;
+// }
+
+// double magic(int number, int rank) //отделяю цифры и складываю
+// {
+//     int result = 0;
+//     for (int i = 1; i <= rank; i++)
+//     {
+//         result += number % Math.Pow(10, i) / Math.Pow(10, i - 1);
+//     }
+
+//     return result;
+// }
+
+
+// void Main()
+// {
+//     Console.Clear();
+//     int number = Input();
+//     int por9dok = Counter(number);
+//     Console.WriteLine(magic(number, por9dok));
+
+// }
+
+// Main();
+
+
+double magic(double number, int rank) //отделяю цифры и складываю
 {
-    int count = 1;
-    for(int del = 10; number / del > 0; del *= 10)
+    double result = 0;
+        
+    for (int i = 1; i <= rank; i++)
     {
-        count ++;
+        // left = number % Math.Pow(10, i);
+        // right = left / Math.Pow(10, i - 1);
+        // result += right;
+        result += number % Math.Pow(10, i) / Math.Pow(10, i - 1);
+        Console.WriteLine(result);
     }
-    return count;
+    return result;
 }
 
-void Main()
-{
-    Console.Clear();
-
-
-}
-
-Main();
+Console.WriteLine(magic(1234, 4));
