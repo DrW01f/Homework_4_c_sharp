3089ba270fe3d8cd311e1abeac042f2031e6e9b8
Andrey <Dr_Wo1f@mail.ru>
2023-02-06 01:29:10 +0300

    task 27 finished

--- task27/Program.cs
+++ task27/Program.cs
@@ -5,60 +5,62 @@
 // 9012 -> 12
 
 
-// int Input()
-// {
-//     Console.WriteLine("Введите число");
-//     int count = int.Parse(Console.ReadLine()!);
-//     return count;
-// }
+int Input()
+{
+    Console.WriteLine("Введите число");
+    int count = int.Parse(Console.ReadLine()!);
+    return count;
+}
 
-// int Counter(int number) //вычисляю порядок числа (начало с 1)
-// {
-//     int count = 1;
-//     for (int del = 10; number / del > 0; del *= 10)
-//     {
-//         count++;
-//     }
-//     return count;
-// }
+int Counter(int number) //вычисляю порядок числа (начало с 1)
+{
+    int count = 1;
+    for (int del = 10; number / del > 0; del *= 10)
+    {
+        count++;
+    }
+    return count;
+}
 
+double magic(int number, int rank) //отделяю цифры и складываю
+{
+    int result = 0;
+
+    for (int i = 1; i <= rank; i++)
+    {
+        result += number % Convert.ToInt32(Math.Pow(10, i)) / Convert.ToInt32(Math.Pow(10, i - 1));
+    }
+
+    return result;
+}
+
+
+void Main()
+{
+    Console.Clear();
+    int number = Input();
+    int por9dok = Counter(number);
+    Console.WriteLine(magic(number, por9dok));
+
+}
+
+Main();
+
+// Console.Clear();
 // double magic(int number, int rank) //отделяю цифры и складываю
 // {
 //     int result = 0;
+
 //     for (int i = 1; i <= rank; i++)
 //     {
-//         result += number % Math.Pow(10, i) / Math.Pow(10, i - 1);
+//         result += number % Convert.ToInt32(Math.Pow(10, i)) / Convert.ToInt32(Math.Pow(10, i - 1));
+//         Console.WriteLine(result);
 //     }
 
 //     return result;
 // }
 
+// double outprint = magic(1234, 4);
 
-// void Main()
-// {
-//     Console.Clear();
-//     int number = Input();
-//     int por9dok = Counter(number);
-//     Console.WriteLine(magic(number, por9dok));
-
-// }
-
-// Main();
-
-
-double magic(double number, int rank) //отделяю цифры и складываю
-{
-    double result = 0;
-        
-    for (int i = 1; i <= rank; i++)
-    {
-        // left = number % Math.Pow(10, i);
-        // right = left / Math.Pow(10, i - 1);
-        // result += right;
-        result += number % Math.Pow(10, i) / Math.Pow(10, i - 1);
-        Console.WriteLine(result);
-    }
-    return result;
-}
-
-Console.WriteLine(magic(1234, 4));
+// Console.WriteLine(1234 % Math.Pow(10, 2) / Math.Pow(10, 2 - 1));
+// Console.WriteLine(34 / 10);
