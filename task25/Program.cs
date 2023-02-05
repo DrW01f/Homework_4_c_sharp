626fcf81e3f2dc70e1bc0985a35a3f8914871565
Andrey <Dr_Wo1f@mail.ru>
2023-02-05 22:10:20 +0300

    task 25 finished

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,26 +1,35 @@
 ﻿// Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // Нельзя использовать библиотеку Math!
 
-long Exponentiation(int count, int rank)
-{
-    int i = 1;
-    while (i <= rank)
-    {
-        count *= count;
-        i++;
-    }
- 
-    return count;
-}
-
-
-void Main()
+int[] Input()
 {
     Console.WriteLine("Введите число");
     int count = int.Parse(Console.ReadLine()!);
     Console.WriteLine("Введите степерь");
     int rank = int.Parse(Console.ReadLine()!);
-     Console.WriteLine(Exponentiation(count, rank));
+    int[] array = { count, rank };
+    return array;
+}
+
+long Exponentiation(int[] array)
+{
+    int i = 1;
+    long result = 1;
+
+    while (i <= array[1]) // возведение в степень
+    {
+        result *= array[0];
+        i++;
+    }
+    return result;
+}
+
+
+void Main()
+{
+    Console.Clear();
+    int[] countRank = Input();
+    Console.WriteLine(Exponentiation(countRank));
 }
 
 Main();
