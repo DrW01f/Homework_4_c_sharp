33c68746ae4308806fc9203ae61a40142a2871d2
Andrey <Dr_Wo1f@mail.ru>
2023-02-05 21:07:37 +0300

    task 25 added

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать библиотеку Math!
+
+long Exponentiation(int count, int rank)
+{
+    int i = 1;
+    while (i <= rank)
+    {
+        count *= count;
+        i++;
+    }
+ 
+    return count;
+}
+
+
+void Main()
+{
+    Console.WriteLine("Введите число");
+    int count = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите степерь");
+    int rank = int.Parse(Console.ReadLine()!);
+     Console.WriteLine(Exponentiation(count, rank));
+}
+
+Main();
